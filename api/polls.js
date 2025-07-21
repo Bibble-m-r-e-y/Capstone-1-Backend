c6163131f020b94951424fc169af5b12d74a3e72
MS <cg.tyler_774@outlook.com>
2025-07-21 15:41:57 -0400

    did delete and the post and the get method

--- api/polls.js
+++ api/polls.js
@@ -2,13 +2,37 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../database");
 const { polls, User } = reqire("../database");
+
+//GET route that retrieves all existing polls
 router.get("/", async (req, res) => {
   try {
     const polls = await polls.findALL(); // promise to grab all the data from the polls table
     res.status(200).send(polls); //send a suscesfull status if done correctly and send me the info.
-  } catch (err) { //catch any errors
+  } catch (err) {
+    //catch any errors
     console.log("error"); //cout errors
   }
 });
 
+//DELETE route to delete poll
+router.delete("/polls/id", async (req, res) => {
+  try {
+    const p = await poll.destroy(); //promise to delete all the poll info
+    res.send(200).send(polls); //delete records from the database
+  } catch (err) {
+    console.log(err, "error");
+  }
+});
+
+
+//update polls?
+router.post("/", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const pO = await poll.findByPk(Id);
+    await pO.save(options);
+  } catch (err) {
+    console.log(err, "error");
+  }
+});
 
