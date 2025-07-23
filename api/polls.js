842b5518e7299a7a6dc1f6356f5cb0cc2e8fa2c9
MS <cg.tyler_774@outlook.com>
2025-07-22 21:56:06 -0400

    did the delete route

--- api/polls.js
+++ api/polls.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../database");
-const { polls, User } = reqire("../database");
+const { Poll, User } = require("../database");
 
 //GET route that retrieves all existing polls
 router.get("/", async (req, res) => {
   try {
-    const polls = await polls.findALL(); // promise to grab all the data from the polls table
+    const polls = await Poll.findAll(); // promise to grab all the data from the polls table
     res.status(200).send(polls); //send a suscesfull status if done correctly and send me the info.
   } catch (err) {
     //catch any errors
@@ -15,24 +14,28 @@ router.get("/", async (req, res) => {
 });
 
 //DELETE route to delete poll
-router.delete("/polls/id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  //create a rout id path
+  const id = Number(req.params.id);
   try {
-    const p = await poll.destroy(); //promise to delete all the poll info
-    res.send(200).send(polls); //delete records from the database
+    const x = await Poll.findByPk(id);
+    console.log(x);
+    await x.destroy(); //promise to delete all the poll info
+    res.sendStatus(200); //delete records from the database
   } catch (err) {
     console.log(err, "error");
+    res.sendStatus(500);
   }
 });
-
 
 //update polls?
 router.post("/", async (req, res) => {
   try {
     const id = req.params.id;
-    const pO = await poll.findByPk(Id);
+    const pO = await poll.findByPk(id);
     await pO.save(options);
   } catch (err) {
     console.log(err, "error");
   }
 });
-
+module.exports = router;
